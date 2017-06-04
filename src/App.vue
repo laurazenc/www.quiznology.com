0dbe1d0023be4120c5fb5a0cc4885fcfcb4d1a0d
Laura Coalla Cencerrado <laurazenc@gmail.com>
2017-06-04 12:25:17 +0200

    starter skeleton using vue-loader

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app" class="hero is-fullheight">
+    <div class="hero-head">
+      <top-nav></top-nav>
+    </div>
+    <div class="hero-body align-content">
+      <router-view></router-view>
+    </div>
+  </div>
+</template>
+
+<script>
+import TopNav from '@/components/common/TopNav';
+
+export default {
+  name: 'app',
+  components: {
+    TopNav,
+  },
+};
+</script>
+
+<style lang="sass">
+  @import '~bulma'
+</style>
+<style lang="scss">  
+  .align-content {
+    align-items: flex-start !important
+  }
+</style>
